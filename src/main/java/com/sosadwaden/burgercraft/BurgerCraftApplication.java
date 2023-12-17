fab8036d288336939cbeae48bee02b61f0324f63
sosadwaden <wadenjap@gmail.com>
2023-12-18 02:58:48 +0300

    0.0.1 version

---
+++ src/main/java/com/sosadwaden/burgercraft/BurgerCraftApplication.java
@@ -0,0 +1,13 @@
+package com.sosadwaden.burgercraft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BurgerCraftApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BurgerCraftApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sosadwaden/burgercraft/BurgerCraftApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sosadwaden.burgercraft;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BurgerCraftApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
